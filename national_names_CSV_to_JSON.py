747227230938ca8b101d3cfe614c75b898c268f0
eweisser <ericwweisser@gmail.com>
2021-11-27 01:53:51 -0600

    first version of CSV to JSON converter for most popular names by year; makes each rank a key, each name a value, which I don't think I want to use

---
+++ national_names_CSV_to_JSON.py
@@ -0,0 +1,52 @@
+import csv
+import json
+
+jsonDict = {}
+top_names_json = []
+
+currentYear = 1931
+recordID = 1
+while currentYear < 2021:
+    with open("national_names/yob" + str(currentYear) + ".txt") as csv_file:
+        recordM = {}
+        recordF = {}
+        csv_reader = csv.reader(csv_file, delimiter=",")
+        recordM["id"] = recordID
+        recordID = recordID + 1
+        recordF["id"] = recordID
+        recordID = recordID + 1
+        recordM["year"] = currentYear
+        recordM["sex"] = "M"
+        recordF["year"] = currentYear
+        recordF["sex"] = "F"
+        rankNumberM = 1
+        rankNumberF = 1
+        for row in csv_reader:
+            if row[1] == "M" and rankNumberM <= 100:
+                if rankNumberM < 10:
+                    keyString = "n0" + str(rankNumberM)
+                else:
+                    keyString = "n" + str(rankNumberM)
+                recordM[keyString] = row[0]
+                # print(rankNumber, keyString, row[0])
+                rankNumberM = rankNumberM + 1
+            if row[1] == "F" and rankNumberF <= 100:
+                if rankNumberF < 10:
+                    keyString = "n0" + str(rankNumberF)
+                else:
+                    keyString = "n" + str(rankNumberF)
+                recordF[keyString] = row[0]
+                rankNumberF = rankNumberF + 1
+        # print(recordM)
+        # print(recordF)
+        top_names_json.append(recordM)
+        top_names_json.append(recordF)
+        # print(top_names_json)
+    currentYear = currentYear + 1
+
+jsonDict["records"] = top_names_json
+print(jsonDict)
+
+# f = open("top_names_json.json", "x")
+# f.write(json.dumps(jsonDict))
+# f.close()
